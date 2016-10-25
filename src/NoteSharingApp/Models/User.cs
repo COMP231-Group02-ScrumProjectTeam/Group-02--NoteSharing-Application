2342cc2c68984ce2533434fc93cc7f267fd6b3f5
Sabrina-Ahmed <sabrinaahmed.bd@gmail.com>
2016-10-24 21:51:00 -0400

    update

--- src/NoteSharingApp/Models/User.cs
+++ src/NoteSharingApp/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,12 +18,17 @@ namespace NoteSharingApp.Models
         }
 
         public int ID { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string College { get; set; }
         public string Enrollement_year { get; set; }
         public string Semester { get; set; }
         public string Program_field { get; set; }
+        [Display(Name = "Email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        //[DataType(DataType.EmailAddress, ErrorMessage = "Error message.")]
         public string User_name { get; set; }
         public string Password { get; set; }
 
