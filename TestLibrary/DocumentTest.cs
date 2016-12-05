2e24f5f47857dcfa05b4ae40ea47aaaa5b742354
Your Name <princess101@gmail.com>
2016-12-04 23:57:58 -0500

    MSUnit Test Cases

---
+++ TestLibrary/DocumentTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NoteSharingApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestLibrary
+{
+    [TestClass]
+    public class DocumentTest
+    {
+        [TestMethod]
+        public bool DownloadAttachmentInt(int id)
+        {
+            Assert.AreEqual(id, 2);
+            return true;
+        }
+        [TestMethod]
+        public bool DownloadAttachmentString(int id)
+        {
+            Assert.AreEqual(id, "aaaaaaaaaa");
+            return true;
+        }
+        [TestMethod]
+        public bool Task(int taskId)
+        {
+            Assert.AreEqual(taskId, 1);
+            return true;
+        }
+    }
+}
